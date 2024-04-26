adbe8eddd4bc0c69f5141385d6152a91c6be3a3d
António Zarco <antonio.zarco.rosario@gmail.com>
2024-04-26 11:13:35 +0100

    Update carousel component and fix props typo in CarouselItem.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,87 @@
 <template>
-  <carousel :slides="slides"></carousel>
+  <div class="app">
+      <carousel >
+          <carousel-slide v-for="slide in slides" :key="slide" class="carousel-slider">
+              <img :src="slide" :alt="slide">
+          </carousel-slide>
+      </carousel>
+  </div>    
 </template>
 
 <script>
-import Carousel from "./components/carousel/Carousel.vue";
+import Carousel from './components/carousel/Carousel.vue';
+import CarouselSlide from './components/carousel/CarouselSlide';
+
 export default {
-  name: "App",
-  components: { Carousel },
-  data: () => ({
-    slides: [
-      "https://picsum.photos/id/1032/900/400",
-      "https://picsum.photos/id/1033/900/400",
-      "https://picsum.photos/id/1034/900/400",
-      "https://picsum.photos/id/1035/900/400",
-      "https://picsum.photos/id/1036/900/400", 
-    ]
-  })
-};
+  data(){
+      return {
+          slides: [
+              'https://picsum.photos/id/230/600/300',
+              'https://picsum.photos/id/231/600/300',
+              'https://picsum.photos/id/232/600/300',
+              'https://picsum.photos/id/233/600/300',
+              'https://picsum.photos/id/234/600/300',
+              'https://picsum.photos/id/235/600/300',
+              'https://picsum.photos/id/236/600/300',
+          ]
+      }
+  },
+
+  components : {
+      Carousel : Carousel,
+      CarouselSlide : CarouselSlide,
+  }
+}
 </script>
 
-<style></style>
+<style>
+  .app {
+      display:flex;
+      justify-content: center;
+  }
+  .carousel {
+      position:relative;
+      overflow: hidden;
+      width:800px;
+      height:500px;
+      z-index:10;
+  }
+  .btn {
+      padding:5px 10px;
+      background-color:rgba(0,0,0,0.5);
+      border:1px solid transparent;
+      margin:5px 10px;
+      color:#FFF;
+      height:50px;
+      width:50px;
+      position:absolute;
+      margin-top:-25px;
+      z-index:2;
+  }
+  .btn:hover {
+      cursor: pointer;
+  }
+  .btn:focus{
+      outline:none;
+  }
+  .btn-next {
+      top:50%;
+      right:0;
+  }
+  .btn-prev {
+      top:50%;
+      left:0;
+  }
+  .carousel-slider {
+      position:absolute;
+      top:0;
+      left:0;
+      bottom:0;
+      right:0;
+  }
+  .carousel-slider img {
+      width:100%;
+      height:100%;
+  }
+
+</style>
--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,33 +1,57 @@
 <template>
-    <div class="carousel">
-        <div class="carousel-inner">
-            <carousel-item 
-            v-for="(slide, index) in slides" 
-            :slide="slide"
-            :key="`item-${index}`"
-            ></carousel-item>
-        </div>
+    <div class="carousel"  @keydown="checkSlide($event)" tabindex="0">
+        <slot></slot>
+        <button @click.prevent="next" class="btn btn-next"><i class="fa fa-angle-right"></i></button>
+        <button @click.prevent="prev" class="btn btn-prev"><i class="fa fa-angle-left"></i></button>
     </div>
 </template>
-
 <script>
-import CarouselItem from "./CarouselItem.vue";
-
 export default {
-    props: ['slides'],
-    components: { CarouselItem },
-};
+    data () {
+        return {
+            index : 0,
+            slides : [],
+            slideDirection: '',
+        }
+    },
+    computed: {
+        slidesLength() {
+            return this.slides.length;
+        }
+    },
+    mounted(){
+        this.slides = this.$children;
+        this.slides.map( (slide,index) => {
+            slide.index = index;
+        });
+    },
+    methods: {
+        next(){
+            this.index++;
+            if(this.index >= this.slidesLength){
+                this.index = 0;
+            }
+            this.slideDirection = 'slide-right';
+        },
+        prev(){
+            this.index--;
+            if(this.index < 0){
+                this.index = this.slidesLength - 1;
+            }
+             this.slideDirection = 'slide-left';
+        },
+        checkSlide(event){
+            if(event.keyCode === 39){
+                this.next();
+            }else if (event.keyCode === 37){
+                this.prev();
+            }else {
+                return;
+            }
+        },
+    }
+}
 </script>
-
-<style scoped>
-.carousel {
-    display: flex;
-    justify-content: center;
-}
-.carousel-inner {
-    position: relative;
-    width: 900px;
-    height: 400px;
-    overflow: hidden;
-}
+<style>
+    
 </style>
